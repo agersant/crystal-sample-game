0fa5557589834e3cb2ed7138f01e60e89f1d9df6
Antoine Gersant <antoine.gersant@lesforges.org>
2023-02-06 22:14:26 -0800

    Let crystal handle package.path

--- runtime/main.lua
+++ runtime/main.lua
@@ -1,5 +1,3 @@
--- TODO Find a way for crystal/init.lua to do this itself
-package.path = package.path .. ";crystal/runtime/?.lua";
 require("crystal/runtime");
 
 love.filesystem.setIdentity("crystal-sample-game");
