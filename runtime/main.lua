d177dfed164b2326136949fb61dbfb87d7eb347b
Antoine Gersant <antoine.gersant@lesforges.org>
2023-02-08 21:52:29 -0800

    Apply fonts and bindings

--- runtime/main.lua
+++ runtime/main.lua
@@ -4,4 +4,24 @@ crystal.configure({
 	assetsDirectory = "assets/"
 });
 
+local Fonts = require("resources/Fonts");
+FONTS = Fonts:new({
+		small = "assets/font/16bfZX.ttf",
+		body = "assets/font/karen2black.ttf",
+		fat = "assets/font/karenfat.ttf",
+	});
+
+INPUT:applyBindings({
+	{
+		left = { "moveLeft" },
+		right = { "moveRight" },
+		up = { "moveUp" },
+		down = { "moveDown" },
+		q = { "interact", "useSkill1", "advanceDialog" },
+		w = { "useSkill2" },
+		e = { "useSkill3" },
+		r = { "useSkill4" },
+	},
+});
+
 love.filesystem.setIdentity("crystal-sample-game");
