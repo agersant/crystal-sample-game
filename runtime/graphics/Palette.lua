60580be534e5d36c9cc7eb35975597b8610f7cf6
Antoine Gersant <antoine.gersant@lesforges.org>
2023-03-25 21:05:00 -0700

    Follow API change

--- runtime/graphics/Palette.lua
+++ runtime/graphics/Palette.lua
@@ -1,8 +1,8 @@
 local Palette = {};
 
-Palette.barbadosCherry = crystal.Color:hex(0xAA0A27);
-Palette.black6C = crystal.Color:hex(0x101820);
-Palette.cyan = crystal.Color:hex(0x00EAFF);
-Palette.strawberry = crystal.Color:hex(0xE72626);
+Palette.barbadosCherry = crystal.Color:new(0xAA0A27);
+Palette.black6C = crystal.Color:new(0x101820);
+Palette.cyan = crystal.Color:new(0x00EAFF);
+Palette.strawberry = crystal.Color:new(0xE72626);
 
 return Palette;
