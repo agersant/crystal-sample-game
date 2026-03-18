9c050def998f504436ef6698afcba10cfb0f5e50
Antoine Gersant <antoine.gersant@lesforges.org>
2026-03-17 21:08:06 -0700

    Adds level script

--- game/arena.lua
+++ game/arena.lua
@@ -1,10 +1,19 @@
 local Arena = Class("Arena", crystal.Scene);
 
+local level_1 = function(self)
+    while true do
+        self:wait(1);
+        self:spawn("Fish");
+    end
+end
+
 Arena.init = function(self)
     self.camera_controller = crystal.CameraController:new();
     self.camera_controller:cut_to(crystal.Camera:new());
 
     self.ecs = crystal.ECS:new();
+    self:add_alias(self.ecs);
+
     self.physics_system = self.ecs:add_system(crystal.PhysicsSystem);
     self.script_system = self.ecs:add_system(crystal.ScriptSystem);
     self.movement_controls_system = self.ecs:add_system("MovementControlsSystem");
@@ -12,6 +21,9 @@ Arena.init = function(self)
 
     self.player = self.ecs:spawn("Player");
     self.platform = self.ecs:spawn("Platform");
+
+    self.level_script = crystal.Script:new(level_1);
+    self.level_script:add_alias(self);
 end
 
 Arena.update = function(self, dt)
@@ -19,6 +31,7 @@ Arena.update = function(self, dt)
     self.physics_system:simulate_physics(dt);
     self.movement_controls_system:apply_movement_controls(dt);
     self.camera_controller:update(dt);
+    self.level_script:update(dt);
     self.script_system:run_scripts(dt);
     self.draw_system:update_drawables(dt);
 end
