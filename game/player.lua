6d1cac108b24e7feda5f952b0eddadf69f75b3e4
Antoine Gersant <antoine.gersant@lesforges.org>
2026-03-24 01:08:30 -0700

    4-direction player sprite

--- game/player.lua
+++ game/player.lua
@@ -5,16 +5,25 @@ Player.init = function(self)
 	self:add_component(crystal.Collider, love.physics.newCircleShape(6));
 	self:set_categories("player");
 
-    self:add_component(crystal.Sprite);
-    local texture = crystal.assets.get("assets/player.png");
+    self:add_component(crystal.AnimatedSprite, crystal.assets.get("assets/hero.json"));
     self:set_texture(texture);
-    self:set_draw_offset(-texture:getWidth() / 2, 4 - texture:getHeight());
     self:set_draw_order_modifier("replace", 1);
 
     self:add_component(crystal.InputListener, 1);
     self:add_component(crystal.Movement);
     self:add_component("MovementControls");
     self:set_speed(75);
+    
+    self:add_component(crystal.ScriptRunner);
+    self:add_script(function(self)
+        while true do
+            local r = self:rotation();
+            local dx, dy = math.angle_to_cardinal(r);
+            local d = dx == 1 and "E" or dx == -1 and "W" or dy == 1 and "S" or "N";
+            self:set_animation("idle", d);
+            self:wait_frame();
+        end
+    end);
 end
 
 return Player;
