a1e11ffc2eb7c06f0cfd653889eb116f692ae282
Antoine Gersant <antoine.gersant@lesforges.org>
2026-03-27 23:35:16 -0700

    Adjust player sprite offset

--- game/player.lua
+++ game/player.lua
@@ -7,7 +7,7 @@ Player.init = function(self)
 
     self:add_component(crystal.AnimatedSprite, crystal.assets.get("assets/hero.json"));
     self:set_texture(texture);
-    self:set_draw_offset(0, -8);
+    self:set_draw_offset(0, -10);
     self:set_draw_order_modifier("replace", 1);
 
     self:add_component(crystal.InputListener, 1);
